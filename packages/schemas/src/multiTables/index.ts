0b9d423120174a389417710f0a0c9d6186f957a6
yangsangho <yangsangho22@gmail.com>
2025-07-31 11:41:02 +0900

    fixed multi table icon

--- packages/schemas/src/multiTables/index.ts
+++ packages/schemas/src/multiTables/index.ts
@@ -3,13 +3,13 @@ import type { MultiTableSchema } from './types.js';
 import { pdfRender } from './pdfRender.js';
 import { uiRender } from './uiRender.js';
 import { propPanel } from './propPanel.js';
-import { Table2 } from 'lucide';
+import { TableCellsSplit } from 'lucide';
 import { createSvgStr } from '../utils.js';
 
 const multiTableSchema: Plugin<MultiTableSchema> = {
   pdf: pdfRender,
   ui: uiRender,
   propPanel,
-  icon: createSvgStr(Table2),
+  icon: createSvgStr(TableCellsSplit),
 };
 export default multiTableSchema;
